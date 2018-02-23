4b0900d3e4852793d025071fd900d3537a56fce3
Samuseva Anna <samuseva.a.s@mail.ru>
2018-02-24 01:52:07 +0300

    H

--- src/index.js
+++ src/index.js
@@ -1,57 +1,16 @@
 
 module.exports = function getZerosCount(number, base) {
-    var prostyeChisla = new Array();
-        var stInSystem = new Array();
-        var stInNumber = new Array();
-
-        for (var i = 2; i < base; i++) {
-            if (base % i == 0) {
-                var prime = true;
-                for (var j = 2; j < i; j++) {
-                    if (i % j == 0)
-                        prime = false;
-                }
-                var k = i, t = 0;
-                if (prime) {
-                    while (base % k == 0) {
-                        k *= i;
-                        t++;
-                    }
-                    prostyeChisla.push(i);
-                    stInSystem.push(t);
-                }
-            }
-        }
-
-        for (var i = 2; i < number; i++) {
-            var tmp = i, n;
-            for (var j = 0; j < prostyeChisla.length; j++) {
-                if (stInNumber.length <= j) {
-                    n = 0;
-                } else {
-                    n = stInNumber[j];
-                }
-                while (tmp % prostyeChisla[j] == 0) {
-                    tmp /= prostyeChisla[j];
-                    n++;
-                }
-                if (stInNumber.length <= j) {
-                    stInNumber.push(n);
-                } else {
-                    stInNumber[j]=n;
-                }
-            }
-
-        }
-
-         num = 0;
-        for (var i = 0; i < prostyeChisla.length; i++) {
-            if (num != 0 && stInNumber[i] / prostyeChisla[i] < num)
-                num = stInNumber[i] / stInSystem[i];
-            if (num == 0) {
-                num = stInNumber[i] / stInSystem[i];
-            }
-        }
-        return num;
+   
+        var count = 0, a=0;
+          for(var i=1; i<=number;i++){
+              a=i;
+              while(a%base==0){
+                  count++;
+                  a=a/base;
+              }
+          }
+          return count;
+      
+   
 }
 
