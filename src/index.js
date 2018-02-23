4234f5cb46042dc68f20cb82d94646f9a8a21dea
Samuseva Anna <samuseva.a.s@mail.ru>
2018-02-23 18:08:17 +0300

    Script

--- src/index.js
+++ src/index.js
@@ -1,22 +1,37 @@
-module.exports = function getZerosCount(number, base) {
-  var count = 0, a=0, pow=0;
-    
-  for(var i=0;i<base;i++){
-    //   var j=2;
-    //   while(j<base){
-        if(base%2==0){
-            base=base/2;
+function prime(number_p){
+    var isPrime=true;
+        var temp = 0;
+        for (var i=2; i<=number_p/2; i++) {
+            temp = number_p % i;
+            if (temp == 0) {
+                isPrime = false;
+                break;
+            }
         }
-        else pow=base;
-    }
-  
-  
-  for(var i=1; i<=number;i++){
-        a=i;
-        while(a%pow==0){
-            count++;
-            a=a/pow;
-        }
-    }
-    return count;
+    return isPrime;
 }
+
+module.exports = function getZerosCount(number, base) {
+    var count = 0, a=0, pow=0;
+    
+    for(var i=2;i<base;i++){
+       if(base%i==0 && prime(base)==false){
+           base=base/i;
+           if(base%i==0){
+               i--;
+           }
+       }
+       else pow=base;
+    }
+
+
+    for(var i=1; i<=number;i++){
+          a=i;
+          while(a%pow==0){
+              count++;
+              a=a/pow;
+          }
+      }
+      return count;
+}
+
--- src/my.js
+++ src/my.js
@@ -1,21 +1,40 @@
-function getZerosCount(number, base) {
+function prime(number_p){
+    var isPrime=true;
+        var temp = 0;
+        for (var i=2; i<=number_p/2; i++) {
+            temp = number_p % i;
+            if (temp == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+    return isPrime;
+}
+
+function getZerosCount(number,base) {
     var count = 0, a=0, pow=0;
     
-    // for(var i=0;i<base;i++){
-    //    if(base%2==0){
-    //        base=base/2;
-    //    }
-    //    else pow=base;
-    // }
-    // console.log(pow);
+    for(var i=2;i<base;i++){
+       if(base%i==0 && prime(base)==false){
+           base=base/i;
+           if(base%i==0){
+               i--;
+           }
+       }
+       else pow=base;
+    }
+    console.log(pow);
+
+
     for(var i=1; i<=number;i++){
           a=i;
-          while(a%19==0){
+          while(a%pow==0){
               count++;
-              a=a/19;
+              a=a/pow;
           }
       }
       return count;
+    // if(prime(base)==false) console.log("false");
   }
-  
-console.log(getZerosCount(15553283,84));
+// getZerosCount(47);
+console.log(getZerosCount(14423541,193));
